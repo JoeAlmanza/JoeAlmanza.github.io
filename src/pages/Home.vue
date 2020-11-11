4e96b1da6cfeb42a726b32944378c3cdd132310b
Joe Almanza <joealmanza2113@gmail.com>
2020-11-11 15:08:05 -0700

    about

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,19 +18,20 @@
           <h2 class="mb-0">About Me</h2>
           <h2 class="">_____</h2>
           <h5 class="mt-5">
-            Joe is a Software Developer who was born and raised in Idaho. His
-            background in Escrow has ingrained a strong sense of importance when
-            it comes to communication, meeting deadlines, thoroughly completing
-            tasks, and providing client satisfaction by keeping them informed
-            while managing hundreds of thousands of their dollars. While Escrow
-            provided a sense of accomplishment when helping people buy or sell
-            property, Joe has never been able to escape the curiousity for
-            software development that comes with being a long time gamer, and
-            growing up watching computers and the internet grow exponentially.
-            Alongside Software Development and video games, Joe also loves
-            animals, cars, clothes, sneakers and fitness. Joe also holds an
-            Associates of Arts Degree in Information Technology from The
-            University of Phoenix.
+            I am a Software Developer who was born and raised in Idaho. With a
+            background in Escrow, I have established skills and experience in
+            handling multiple transactions within limited time constraints,
+            while still providing exceptional client satisfaction. While Escrow
+            gave me a sense of accomplishment, I have never been able to escape
+            the curiousity for programming that comes with being a long time
+            gamer, and growing up watching computers and the internet grow
+            exponentially. Alongside software development and video games, I
+            love spending time with my cats, working on my car, and finding new
+            music to listen to. Learning software development has ignited the
+            spark of curiousity and I'm looking forward to learning as much as I
+            can as well as applying my skills to build more applications I'm
+            proud of. I also hold an Associates of Arts Degree in Information
+            Technology from The University of Phoenix.
           </h5>
         </div>
       </div>
@@ -102,7 +103,7 @@
           <h5 class="my-5">If you like what you see, let me know!</h5>
         </div>
 
-        <div class="col-3 pt-4 mx-0 text-center">
+        <div class="col-lg-3 pt-4 mx-0 text-center">
           <a class="my-4" :href="'mailto:JoeAlmanza2113@gmail.com'">
             <i
               class="fa fa-envelope-o contactImg text-secondary"
@@ -113,7 +114,7 @@
           </a>
         </div>
 
-        <div class="col-3 pt-4 mx-0 text-center">
+        <div class="col-lg-3 pt-4 mx-0 text-center">
           <a
             class="my-4"
             onclick="location.href='https://www.linkedin.com/in/joseph-almanza/';"
@@ -127,7 +128,7 @@
           </a>
         </div>
 
-        <div class="col-3 pt-4 mx-0 text-center">
+        <div class="col-lg-3 pt-4 mx-0 text-center">
           <a
             class="my-4"
             onclick="location.href='https://joealmanza.github.io/Joe-Almanza/';"
@@ -174,6 +175,7 @@ strong {
 }
 img {
   max-height: 40vh;
+  max-width: 100%;
 }
 .appRow {
   background-color: #040524;
